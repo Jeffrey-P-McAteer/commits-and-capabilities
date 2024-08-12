6d005ace33d99fc0c27f621f78e671d13943712d
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-08-12 17:24:11 -0400

    meh

---
+++ commits-and-capabilities.py
@@ -0,0 +1,31 @@
+
+import os
+import sys
+import shutil
+import time
+
+import tkinter
+
+
+
+
+
+
+
+def main(args=sys.argv):
+  window = tkinter.Tk()
+  window.title("Hello World")
+
+  window.mainloop()
+
+
+
+if __name__ == '__main__':
+  main()
+
+
+
+
+
+
+
